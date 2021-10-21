be354dae602b98d37531ed839a43db96f31817aa
151090 <m.helmy.amit@gmail.com>
2021-10-21 11:23:57 +0200

    testsjhfdjhbfjhv

--- main.c
+++ main.c
@@ -6,16 +6,6 @@ int main()
     int x=10;
     int*p_1=&x;
     int**p_2=&p_1;
-    //printf("%d",x);
-    //printf("%d\n",p_1);
-    //printf("%d",*p_1);
-    //printf("%d",p_2);
-    //printf("%d",*p_2);
-    //printf("%d",**p_2);
-    //*p_1=20;
-    //printf("%d\n",x);
-
-    //**p_2=30;
-    //printf("%d\n",x);
+  
     return 0;
 }
