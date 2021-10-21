7337a87626e12a030ca6f5f6241ffbd398929578
151090 <m.helmy.amit@gmail.com>
2021-10-21 11:32:40 +0200

    testing new branch

---
+++ main (2).c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+int main()
+{
+    int x = 8;
+    if(x==1)                           // Try if(x=0)
+    {
+    printf("Hello world!\n");
+    }
+    printf("%d",x);
+    return 0;
+}
+*/
+/*
+int main()
+{
+    int x=6;
+    if(x==6)                            //remove ; and see diff.
+    printf("Hello world!\n");
+    printf("Hello world_2!\n");
+    return 0;
+}
+
+*/
