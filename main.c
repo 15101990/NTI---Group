76cf7254b86828e3eeec63112534bdeba48fe073
151090 <m.helmy.amit@gmail.com>
2021-10-21 10:23:48 +0200

    test

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x=10;
+    int*p_1=&x;
+    int**p_2=&p_1;
+    //printf("%d",x);
+    //printf("%d\n",p_1);
+    //printf("%d",*p_1);
+    //printf("%d",p_2);
+    //printf("%d",*p_2);
+    //printf("%d",**p_2);
+    //*p_1=20;
+    //printf("%d\n",x);
+
+    //**p_2=30;
+    //printf("%d\n",x);
+    return 0;
+}
